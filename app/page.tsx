eaa372589a26b755738582789505c20a56c66c55
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-07-31 20:10:35 +0100

    feat removed sign in for access

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import { useAuth } from "@clerk/nextjs";
 export default function MusicPage() {
   const { userId } = useAuth();
 
-  if (userId) {
-    return redirect("/music");
-  }
+  // if (userId) {
+  //   return redirect("/music");
+  // }
 
-  return redirect("/sign-in");
+  return redirect("/music");
 }
